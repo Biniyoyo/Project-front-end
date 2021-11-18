c1261ba3c23f9fba66c510d6bd24475c18dbd9d3
Kyungbae Min <kbmin1129@gmail.com>
2021-11-18 21:15:26 +0900

    optimization

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,34 @@
 import React from "react";
-import {Link,BrowserRouter as Router} from 'react-router-dom';
-import '../css/navbar.css';
-import Profile from "./Profile";
+import { Link, BrowserRouter as Router } from "react-router-dom";
+import "../css/navbar.css";
+import Profile from "../pages/Profile";
 
-function Navebar(){
-
-    return (
-    <div className="navbar">
-        <h1 className="title">Day Logger</h1>
-        <div className="links">
-        <h3><Link className="link" to='/log'>Log Day</Link></h3>
-        <h3><Link className="link" to='/edit'>Edit Questions</Link></h3>
-        <h3><Link className="link" to='/view'>View Data</Link></h3>
-        </div>
-        <Link className="link" to='/profile'><img className="profileImage" src = "/profile.png" alt=""
-            ></img> </Link>
-    </div>
-    )
-    
+function Navebar() {
+	return (
+		<div className="navbar">
+			<h1 className="title">Day Logger</h1>
+			<div className="links">
+				<h3>
+					<Link className="link" to="/log">
+						Log Day
+					</Link>
+				</h3>
+				<h3>
+					<Link className="link" to="/edit">
+						Edit Questions
+					</Link>
+				</h3>
+				<h3>
+					<Link className="link" to="/view">
+						View Data
+					</Link>
+				</h3>
+			</div>
+			<Link className="link" to="/profile">
+				<img className="profileImage" src="/profile.png" alt=""></img>{" "}
+			</Link>
+		</div>
+	);
 }
 
 export default Navebar;
